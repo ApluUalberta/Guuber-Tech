2662a7d769c3d6cec1478e2125b5ece31d858c25
lorenpike <noahceverett@gmail.com>
2023-01-11 20:37:55 -0700

    Some random changes around the db.

--- gt-backend/blog_api/__init__.py
+++ gt-backend/blog_api/__init__.py
@@ -1,3 +1,3 @@
-from .api import app
-
 __version__ = '0.1.0'
+
+from .api import app
--- gt-backend/blog_api/database.py
+++ gt-backend/blog_api/database.py
@@ -1,19 +1,29 @@
 import sqlite3 as sql
 from pathlib import Path
 
+from pydantic import BaseModel
+
 
 DB_PATH = Path("guuber.db")
 SQL_COMMANDS_PATH = Path(__file__).parent / "sql"
+
+
+class Blog(BaseModel):
+    ...
+
+class SQLCommands:
+    def __getattr__(self, name):
+        path = SQL_COMMANDS_PATH / f"{name}.sql"
+        return path.read_text()
+sql_commands = SQLCommands()
+
 
 def create_database():
     """Returns a database connection."""
     conn = sql.connect(str(DB_PATH))
     cur = conn.cursor()
-    cur.execute(get_sql_string("create_blogs"))
+    cur.execute(sql_commands.create_blogs)
     return conn
 
-def get_sql_string(name):
-    path = SQL_COMMANDS_PATH / f"{name}.sql"
-    return path.read_text()
         
     
