87e5dce5d8ba474a1af0444e67c19d4b73a1f58f
lorenpike <noahceverett@gmail.com>
2023-01-22 20:05:34 -0700

    Just a demo app, because Noah has no clue how to use svelte

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,42 @@
 <script lang="ts">
-	export let name: string;
+  import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+  import FeedbackForm from "./components/FeedbackForm.svelte";
+
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu est in turpis egestas cursus id ac purus. Sed malesuada tortor quis efficitur porttitor. Suspendisse dapibus, tellus sit amet viverra placerat, justo nulla vulputate diam, at accumsan tortor eros nec neque. Praesent nisi risus, fermentum eu placerat eu, elementum vitae sapien.",
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: "Ut tristique lorem ut lacus suscipit, et malesuada nisi fermentum. Sed condimentum placerat ipsum at molestie. Pellentesque eros purus, accumsan id tincidunt quis, bibendum a orci. Donec et leo eu quam suscipit lobortis ut vel mi.",
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: "Morbi vel dictum purus. Vestibulum dignissim viverra tellus quis ultricies. Aenean non mauris id massa efficitur finibus sed facilisis massa. Ut tempor odio et condimentum ornare. Morbi luctus cursus sapien, a auctor justo dignissim ut. Duis scelerisque sodales leo, aliquet vestibulum metus pharetra ut. Duis tincidunt, odio sit amet varius maximus, dolor sem vehicula enim, vitae ullamcorper odio metus non nibh.",
+    },
+  ];
+
+  const deleteFeedback = (e) => {
+    let itemId: number = e.detail;
+    feedback = feedback.filter((item) => item.id !== itemId);
+  };
+
+  const addFeedback = (e) => {
+	let newFeedback = e.detail;
+	feedback = [newFeedback, ...feedback]
+  }
+
+  $: count = feedback.length;
+  $: average = feedback.reduce((a, { rating }) => a + rating, 0) / count;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+  <FeedbackForm on:add-feedback={addFeedback} />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feeback={deleteFeedback} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ frontend/src/components/FeedbackList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { fade, scale } from "svelte/transition";
+  import FeedbackItem from "./FeedbackItem.svelte";
+
+  export let feedback = [];
+</script>
+
+{#each feedback as fb (fb.id)}
+  <div in:scale out:fade="{{duration: 500}}">
+    <FeedbackItem item={fb} on:delete-feeback />
+  </div>
+{/each}
